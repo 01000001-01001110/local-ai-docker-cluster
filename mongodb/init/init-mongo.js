8e6290d6f4725318338de6dc6b7a5440780253ca
Alan Newingham <alan.newingham@gmail.com>
2024-12-08 19:12:19 -0500

    Update

---
+++ mongodb/init/init-mongo.js
@@ -0,0 +1,48 @@
+﻿print('Starting MongoDB initialization...');
+
+try {
+    // Wait for MongoDB to be ready
+    sleep(5000);
+
+    // Switch to admin database
+    db = db.getSiblingDB('admin');
+
+    // Create root user if it doesn't exist
+    if (!db.getUser(process.env.MONGODB_USER)) {
+        db.createUser({
+            user: process.env.MONGODB_USER,
+            pwd: process.env.MONGODB_PASSWORD,
+            roles: ['root']
+        });
+    }
+
+    // Switch to langchain database
+    db = db.getSiblingDB(process.env.MONGODB_DATABASE);
+
+    // Create collections if they don't exist
+    if (!db.getCollectionNames().includes('vectors')) {
+        db.createCollection('vectors');
+        print('Created vectors collection');
+    }
+
+    // Create vector search index
+    db.vectors.createIndex(
+        {
+            "embedding": {
+                "type": "vectorSearch",
+                "numDimensions": 1536,
+                "similarity": "cosine"
+            }
+        },
+        {
+            name: "vector_index"
+        }
+    );
+    print('Created vector search index');
+
+} catch (err) {
+    print('Error during initialization: ' + err);
+    throw err;
+}
+
+print('MongoDB initialization complete');
